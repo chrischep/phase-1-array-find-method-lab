55b700d79125d0a544ba2b48a501cf8246db0a46
chrischep <christine.chepleting@student.moringaschool.com>
2022-08-23 11:07:53 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const rec = [{
+year: "2015",
+result: "l"},
+{year: "2014",
+result: "n/a"},
+{year: "2013",
+result: "l"},]
+function superbowlWin(Array) {
+const rec = Array.find(element => element.result === "W");
+if (typeof rec === 'object') {
+    return rec.year;
+}
+} 
